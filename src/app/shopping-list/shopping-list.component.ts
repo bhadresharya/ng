8de67ff60a5b6401f0ffbddca1d1c093e1afd56c
bhadresharya <bhadresharya@hotmail.com>
2021-06-06 04:07:37 +0530

    ingredients modal & shopping list details

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../shared/ingredients.modal';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredients[] = [
+    new Ingredients('Apple', 10),
+    new Ingredients('Banana', 5),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
