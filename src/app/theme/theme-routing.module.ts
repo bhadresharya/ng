609999b8c4ee725495ef0838071d786988af57f6
Bhadresh Ranpara <bhadresh.ranpara@marlabs.com>
2021-07-29 18:05:12 +0530

    buttons, typography and page-header

--- src/app/theme/theme-routing.module.ts
+++ src/app/theme/theme-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ButtonsComponent } from './buttons/buttons.component';
 import { ThemeComponent } from './theme.component';
+import { TypographyComponent } from './typography/typography.component';
 
-const routes: Routes = [{ path: '', component: ThemeComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: ThemeComponent,
+    children: [
+      {
+        path: 'buttons',
+        component: ButtonsComponent
+      },
+      {
+        path: 'typography',
+        component: TypographyComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/theme/theme.module.ts
+++ src/app/theme/theme.module.ts
@@ -5,13 +5,19 @@ import { ThemeRoutingModule } from './theme-routing.module';
 import { ThemeComponent } from './theme.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { TypographyComponent } from './typography/typography.component';
+import { PageHeaderComponent } from './layout/page-header/page-header.component';
 
 
 @NgModule({
   declarations: [
     ThemeComponent,
     SidebarComponent,
-    HeaderComponent
+    HeaderComponent,
+    ButtonsComponent,
+    TypographyComponent,
+    PageHeaderComponent
   ],
   imports: [
     CommonModule,
