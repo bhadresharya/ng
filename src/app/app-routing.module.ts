5a19f9e3a67c22a493096dfbcf77efb9e7800ffd
Bhadresh Ranpara <Bhadresh.Ranpara@marlabs.com>
2021-08-10 15:46:52 +0530

    login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/login/login.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'theme' },
+  { path: 'login', component: LoginComponent },
   { path: 'theme', loadChildren: () => import('./modules/theme/theme.module').then(m => m.ThemeModule) }
 ];
 
